472c56282230da0f21bb6c47e4ee1e626899dc38
Simon Weis <laguna.ws@gmail.com>
2023-02-25 06:21:49 +0100

    Add underlying grid

--- impl/gamelib/grid.cpp
+++ impl/gamelib/grid.cpp
@@ -1,8 +1,11 @@
 #include "grid.hpp"
+#include <pathfinder/node.hpp>
+#include <drawable_helpers.hpp>
 #include <game_interface.hpp>
+#include <game_properties.hpp>
 #include <math_helper.hpp>
 
-void Grid::doCreate() { }
+void Grid::doCreate() { createTiles();}
 void Grid::doUpdate(float const elapsed)
 {
     if (getGame()->input().mouse()->justPressed(jt::MouseButtonCode::MBLeft)) {
@@ -36,13 +39,71 @@ void Grid::doUpdate(float const elapsed)
     for (auto& shp : m_shapes) {
         shp->update(elapsed);
     }
+    for(auto& tile : m_tiles)
+    {
+        tile->getDrawable()->update(elapsed);
+    }
 }
 void Grid::doDraw() const
 {
+    for(auto& tile : m_tiles)
+    {
+        tile->getDrawable()->draw(renderTarget());
+    }
+
     for (auto& shp : m_shapes) {
         shp->draw(renderTarget());
     }
     if (m_currentShape) {
         m_currentShape->draw(renderTarget());
     }
+}
+std::shared_ptr<jt::tilemap::TileNode> Grid::getTileAt(int x, int y)
+{
+    if (x < 0) {
+        return nullptr;
+    }
+    if (y < 0) {
+        return nullptr;
+    }
+    if (x >= m_mapSizeX) {
+        return nullptr;
+    }
+    if (y >= m_mapSizeY) {
+        return nullptr;
+    }
+    int const index = y + x * m_mapSizeY;
+    if (static_cast<std::size_t>(index) >= m_tiles.size()) {
+        return nullptr;
+    }
+    return m_tiles.at(index);
+}
+void Grid::createTiles() {
+
+    float const distX = GP::GetScreenSize().x / m_mapSizeX;
+    float const distY = GP::GetScreenSize().y / m_mapSizeY;
+    for (int i = 0; i != m_mapSizeX; ++i) {
+        for (int j = 0; j != m_mapSizeY; ++j) {
+            std::shared_ptr<jt::Shape> drawable = jt::dh::createShapeCircle(
+                distX/10, jt::colors::White, textureManager());
+            drawable->setPosition(jt::Vector2f { i * distX, j * distY });
+            auto node = std::make_shared<jt::pathfinder::Node>();
+            node->setPosition(
+                jt::Vector2u { static_cast<unsigned int>(i), static_cast<unsigned int>(j) });
+
+            m_tiles.emplace_back(std::make_shared<jt::tilemap::TileNode>(drawable, node));
+        }
+    }
+
+    for (int i = 5; i != 10; ++i) {
+        getTileAt(i, 6)->setBlocked(true);
+    }
+    getTileAt(5, 0)->setBlocked(true);
+    getTileAt(5, 1)->setBlocked(true);
+    getTileAt(5, 2)->setBlocked(true);
+    getTileAt(5, 3)->setBlocked(true);
+    getTileAt(5, 4)->setBlocked(true);
+    getTileAt(5, 5)->setBlocked(true);
+
+    getTileAt(9, 5)->setBlocked(true);
 }
--- impl/gamelib/grid.hpp
+++ impl/gamelib/grid.hpp
@@ -6,17 +6,25 @@
 #include <vector.hpp>
 #include <memory>
 #include <vector>
+#include <tilemap/tile_node.hpp>
 
 class Grid : public jt::GameObject {
 public:
+    std::shared_ptr<jt::tilemap::TileNode> getTileAt(int x, int y);
 private:
-    std::vector<std::shared_ptr<jt::Shape>> m_shapes;
+    int const m_mapSizeX = 15;
+    int const m_mapSizeY = 10;
+    std::vector<std::shared_ptr<jt::Shape>> m_shapes {};
     std::shared_ptr<jt::Shape> m_currentShape { nullptr };
     jt::Vector2f m_shapeStart { 0.0f, 0.0f };
+
+    std::vector<std::shared_ptr<jt::tilemap::TileNode>> m_tiles{};
 
     void doCreate() override;
     void doUpdate(float const elapsed) override;
     void doDraw() const override;
+
+    void createTiles();
 };
 
 #endif // ALAKAJAM17_GRID_HPP
